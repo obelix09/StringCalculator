867991e9d8e75bcb242d204a3f58fd68ae723f3c
Petra Frantz <petra17.ru.is>
2018-10-10 23:46:39 +0000

    calculator returns a number when string only has one number

--- src/calculator.js
+++ src/calculator.js
@@ -3,6 +3,9 @@ function add(number) {
 	if (number == "") {
 		return 0;
 	}
+
+	var num = parseInt(number);
+	return num;
 }
 
 module.exports = add;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,3 +3,7 @@ const calculator = require('./calculator');
 it("should return 0 when string is empty", () => {
 	expect(calculator("")).toBe(0);
 });
+
+it("should return a number when string has only one number", () => {
+	expect(calculator("3")).toBe(3);
+});
